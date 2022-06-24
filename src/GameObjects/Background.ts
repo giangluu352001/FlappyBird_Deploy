128f35f67dd00ca5bddc9afd0e99bda4b611e1bc
giangluu352001 <89308367+giangluu352001@users.noreply.github.com>
2022-06-24 18:16:29 +0700

    Delete src directory

--- src/GameObjects/Background.ts
+++
@@ -1,9 +0,0 @@
-import Phaser = require("phaser");
-
-export class Background extends Phaser.GameObjects.Image {
-    constructor(scene: Phaser.Scene, x: number, y: number, scale?: number) {
-        super(scene, x, y, 'background');
-        this.scene.add.existing(this);
-        this.setScale(scale).setDepth(1);
-    }
-}
